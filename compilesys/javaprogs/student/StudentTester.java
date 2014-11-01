de51ae957f8ba1f152625f419eee78e04827605a
mallouk <jalloukmat@yahoo.com>
2014-11-01 16:54:28 -0400

    removed unsupported files/folders

--- compilesys/javaprogs/student/StudentTester.java
+++
@@ -1,9 +0,0 @@
-public class StudentTester
-{
-    public static void main(String[] args)
-        {
-            Student s1 = new Student("Joe Istten", 39);
-            System.out.println("Name = " + s1.getName());
-            System.out.println("Age = " + s1.getAge());            
-        }
-}
