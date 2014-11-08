aefbf9d943228f7f1f961c269d1efcbf00c678dc
mallouk <jalloukmat@yahoo.com>
2014-11-07 20:09:53 -0500

    Removed python and fixed spelling errors

--- compilesys/python/helloworld/helloWorld.py
+++
@@ -1,5 +0,0 @@
-print("Hello World");
-
-x = 3
-
-print(x);
--- compilesys/python/input/input.py
+++
@@ -1,5 +0,0 @@
-name = raw_input("Enter a name please: ");
-print("Your name is: " + name);
-
-# raw_input deals with strings
-#     input deals with integers or numbers
